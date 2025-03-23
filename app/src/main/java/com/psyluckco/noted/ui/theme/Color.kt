307e1a09fdb8a4ed717542a6b2cbc867965166fa
adarshshetty <adarshshetty787@gmail.com>
2025-03-23 17:55:58 +0530

    Commits made: - Changed theme. - Added navigation actions.

--- app/src/main/java/com/psyluckco/noted/ui/theme/Color.kt
+++ app/src/main/java/com/psyluckco/noted/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val AppBlue = Color(0xFF5C6BC0)
+val AppBlueGrey = Color(0xFF3F51B5)
+val AppAqua = Color(0xFF03A9F4)
